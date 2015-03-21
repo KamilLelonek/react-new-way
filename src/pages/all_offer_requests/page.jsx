8bb6dedee04ee157889e309a184bca01c032875c
Kamil Lelonek <squixy.sln@gmail.com>
2015-03-21 21:30:06 +0100

    Rendering domain models with components

--- src/pages/all_offer_requests/page.jsx
+++ src/pages/all_offer_requests/page.jsx
@@ -1,23 +1,32 @@
-import React from "react";
-import { getData } from "../../common/request";
+import React                 from "react";
+import { getData }           from "../../common/request";
+import OfferRequest          from "./models/offer_request"
+import OfferRequestComponent from "./components/offer_request_component"
 
-export default class AllOfferRequests extends React.Component {
+class AllOfferRequests extends React.Component {
   static fetchData() {
     return getData("/offer_requests");
   }
 
   componentWillMount() {
-    console.log("[AllOfferRequests] will mount with server response: ", this.props.data);
+    this.offerRequests = this.props.data["offer-requests"].map(
+      offerRequestJsonObject => {
+        let offerRequest = OfferRequest.buildFromJson(offerRequestJsonObject.dump);
+        return <OfferRequestComponent key={ offerRequest.id } offerRequest={ offerRequest } />;
+      }
+    )
   }
 
   render() {
-    let offerRequests = this.props.data["offer-requests"];
-    let title = offerRequests;
-
     return (
-      <div id="offer-requests-page">
-        <h1>{ title }</h1>
+      <div>
+        { this.offerRequests }
       </div>
     );
   }
 };
+
+AllOfferRequests.propTypes    = { data: React.PropTypes.object };
+AllOfferRequests.defaultProps = { "offer-requests": [] };
+
+export default AllOfferRequests;
