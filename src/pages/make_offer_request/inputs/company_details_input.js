161f3017d87922c05de1e4eeebb58cf4ab09ec07
Kamil Lelonek <squixy.sln@gmail.com>
2015-03-24 18:50:08 +0100

    Cleaning up HTML a little bit

--- src/pages/make_offer_request/inputs/company_details_input.js
+++ src/pages/make_offer_request/inputs/company_details_input.js
@@ -4,27 +4,25 @@ export default class CompanyDetailsInput extends React.Component {
   render() {
     return (
       <div className="row">
-        <form className="col s12">
-          <div className="row">
-            <div className="input-field col s6">
-              <i className="mdi-editor-format-color-text prefix"></i>
-              <input id="company_name" type="text" className="validate" />
-              <label htmlFor="company_name">Company Name</label>
-            </div>
-            <div className="input-field col s6">
-              <i className="mdi-communication-phone prefix"></i>
-              <input id="company_phone" type="text" className="validate" />
-              <label htmlFor="company_phone">Company Phone</label>
-            </div>
+        <div className="row">
+          <div className="input-field col s6">
+            <i className="mdi-editor-format-color-text prefix"></i>
+            <input id="company_name" type="text" className="validate" />
+            <label htmlFor="company_name">Company Name</label>
           </div>
-          <div className="row">
-            <div className="input-field col s12">
-              <i className="mdi-communication-business prefix"></i>
-              <input id="address" type="text" className="validate" />
-              <label htmlFor="address">Address</label>
-            </div>
+          <div className="input-field col s6">
+            <i className="mdi-communication-phone prefix"></i>
+            <input id="company_phone" type="text" className="validate" />
+            <label htmlFor="company_phone">Company Phone</label>
           </div>
-        </form>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <i className="mdi-communication-business prefix"></i>
+            <input id="company_address" type="text" className="validate" />
+            <label htmlFor="company_address">Address</label>
+          </div>
+        </div>
       </div>
     );
   }
--- src/pages/make_offer_request/inputs/customer_details_input.js
+++ src/pages/make_offer_request/inputs/customer_details_input.js
@@ -4,20 +4,16 @@ export default class CustomerDetailsInput extends React.Component {
   render() {
     return (
       <div className="row">
-        <form className="col s12">
-          <div className="row">
-            <div className="input-field col s6">
-              <i className="mdi-action-account-circle prefix"></i>
-              <input id="icon_prefix" type="text" className="validate" />
-              <label htmlFor="icon_prefix">Customer Name</label>
-            </div>
-            <div className="input-field col s6">
-              <i className="mdi-communication-email prefix"></i>
-              <input id="icon_email" type="tel" className="validate" />
-              <label htmlFor="icon_email">Customer Email</label>
-            </div>
-          </div>
-        </form>
+        <div className="input-field col s6">
+          <i className="mdi-action-account-circle prefix"></i>
+          <input id="customer_name" type="text" className="validate" />
+          <label htmlFor="customer_name">Customer Name</label>
+        </div>
+        <div className="input-field col s6">
+          <i className="mdi-communication-email prefix"></i>
+          <input id="customer_email" type="tel" className="validate" />
+          <label htmlFor="customer_email">Customer Email</label>
+        </div>
       </div>
     );
   }
