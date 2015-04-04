e9cca98bcad62a8186acdd15525ca02d179f87bc
Kamil Lelonek <squixy.sln@gmail.com>
2015-04-04 12:57:07 +0200

    Service for making offer request

--- src/pages/make_offer_request/services/make_offer_request.js
+++ src/pages/make_offer_request/services/make_offer_request.js
@@ -1,9 +1,31 @@
+import { postData } from "../../../common/request";
+
 export default class MakeOfferRequestService {
   constructor(customerDetails, companyDetails, products, deliveryID) {
-
+    this.customerDetails = customerDetails
+    this.companyDetails  = companyDetails
+    this.products        = products
+    this.deliveryID      = deliveryID
   }
 
   call() {
+    return postData("/offer_requests", this.jsonRequestBody())
+  }
 
+  jsonRequestBody() {
+    return {
+      customer_name:   this.customerDetails.name,
+      customer_email:  this.customerDetails.email,
+      delivery_id:     this.deliveryID,
+      company_name:    this.companyDetails.name,
+      company_phone:   this.companyDetails.phone,
+      company_address: this.companyDetails.address,
+      products:        this.products.map(product => {
+        return {
+          quantity:    product.quantity,
+          category_id: product.category
+        }
+      })
+    }
   }
 }
